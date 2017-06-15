6092ddb82a184698d363144d53c68cefe1d53364
Steve Hicks <steveh@forge-tech.com>
2017-06-15 11:13:45 -0400

    finished highest and lowest

--- Kata_Highest_And_Lowest/solution.py
+++ Kata_Highest_And_Lowest/solution.py
@@ -1,6 +1,7 @@
 #! python3
 
-# In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+# In this little assignment you are given a string of space separated numbers,
+# and have to return the highest and lowest number.
 
 # Example:
 
@@ -10,5 +11,17 @@
 
 
 def high_and_low(numbers):
-    # ...
+    newNumbers = numbers.split()
+    lowest = 0
+    highest = 0
+    for num in newNumbers:
+        if int(num) < lowest or lowest == 0:
+            lowest = int(num)
+
+        if int(num) > highest or highest == 0:
+            highest = int(num)
+
+    numbers = str(highest) + ' ' + str(lowest)
     return numbers
+
+print(high_and_low("1 2 -11 2 3 4 5"))
