1b414d64f3e266dfaf79b44a8cb6637e4e9c7c3e
Steven <shicks255@yahoo.com>
2017-07-27 16:16:08 -0400

    check for lost number between 2 arrays

---
+++ Kata_Check_For_Prime_Numbers/Kata_Lost_Number_in_Number_Sequence/solution.py
@@ -0,0 +1,28 @@
+# !python3
+
+# An ordered sequence of numbers from 1 to N is given.
+# One number might have deleted from it, then the remaining numbers were mixed.
+# Find the number that was deleted.
+#
+# Example:
+#
+# The starting array sequence is [1,2,3,4,5,6,7,8,9]
+# The mixed array with one deleted number is [3,2,4,6,7,8,1,9]
+# Your function should return the int 5.
+# If no number was deleted from the array and no difference with it, your function should return the int 0.
+#
+# Note that N may be 1 or less (in the latter case, the first array will be []).
+
+def find_deleted_number(arr, mixed_arr):
+    mixed_arr.sort()
+    missingNumber = 0
+
+    for x in arr:
+        if x not in mixed_arr:
+            missingNumber = x
+            break
+
+    return missingNumber
+
+print(find_deleted_number([1, 2, 3, 4, 5], [3, 4, 1, 5]))
+
