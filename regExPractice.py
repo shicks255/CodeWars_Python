0617ed90b5115df6a40d84fea8938c19c9d8b0ca
Steven <shicks255@yahoo.com>
2017-08-04 16:52:38 -0400

    trimFile script

--- regExPractice.py
+++ regExPractice.py
@@ -2,17 +2,9 @@
 
 import re
 
-
-oldTitle = 'Black Sails S01E01 HDTV.xvid-AFG.avi';
-
-regex = re.compile(r'(\sHDTV\.xvid\-AFG)')
-
-mo = regex.search(oldTitle)
-mo.group()
-no = regex.findall(oldTitle)
-
-newTitle = oldTitle.replace(mo.group(), '')
-
-print(mo.group())
-print(mo.group(0))
-print(no)
+regex = re.compile()
+mo = regex.search()
+if mo:
+    newName = re.sub('\s+', ' ', )
+    newName = re.sub('\s+[.]', '.', )
+    newName = newName.lstrip()
