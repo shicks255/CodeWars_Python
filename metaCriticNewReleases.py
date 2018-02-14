a356d6953028d704602dd2d1c8ef9321eff59e71
shicks <shicks255@yahoo.com>
2018-02-14 15:38:41 -0500

    working on meta critic new release program

--- metaCriticNewReleases.py
+++ metaCriticNewReleases.py
@@ -2,7 +2,30 @@
 
 import requests
 import bs4
-import os
+
+
+class Release(object):
+    artist = ""
+    album = ""
+    releaseDate = ""
+    score = ""
+
+    # Constructor
+    def __init__(self, artist, album, releaseDate, score):
+        self.artist = artist
+        self.album = album
+        self.releaseDate = releaseDate
+        self.score = score
+
+    # to string
+    def __str__(self):
+        return str(self.__dict__)
+
+
+def make_release(artist, album, releaseDate, score):
+    release = Release(artist.strip(), album.strip(), releaseDate.strip(), score.strip())
+    return release
+
 
 url = 'http://www.metacritic.com/browse/albums/release-date/new-releases/date'
 res = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
@@ -11,9 +34,8 @@ soup = bs4.BeautifulSoup(res.text, "html.parser")
 
 entries = soup.select('.product_wrap')
 
-releases = [entries.__sizeof__()]
-
-# file = os.open("newReleases.txt", 0)
+listOfNewReleases = []
+file = open("newReleases.txt", "a+")
 
 for entry in entries:
     album = ""
@@ -34,5 +56,13 @@ for entry in entries:
                 if 'release_date' in item.get('class'):
                     releaseDate = item.find('span', {"class": "data"}).getText()
 
-    print(artist.strip() + " " + album.strip() + " " + score.strip() + " " + releaseDate.strip())
+    release = make_release(artist, album, releaseDate, score)
+
+    if str(release) not in open('newReleases.txt').read():
+        file.write(str(release)+"\n")
+        listOfNewReleases.append(release)
+
+
+
+print(len(listOfNewReleases))
 
