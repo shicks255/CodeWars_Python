59315e369aa6043394b3c195185201354dd41ec3
Hicks <steveh@genesisedu.com>
2018-02-15 23:23:38 -0500

    started the forge import thing

--- forgeImportDistrict.py
+++ forgeImportDistrict.py
@@ -7,8 +7,24 @@ import os
 import datetime
 from selenium import webdriver
 
+def get_district_url(notecards):
+    for card in notecards:
+        if 'notecard' in card.get('class'):
+            if 'SchoolFi' in card.find('b').getText():
+                districts = card.find_all('tr')
+                for district in districts:
+                    if district.get('data-type'):
+                        if 'customer' in district.get('data-type'):
+                            if district.find('a'):
+                                districtName = district.find('a').getText()
+                                if searchTerms.lower() in districtName.lower():
+                                    if (district.find('a').get('href')):
+                                        url = district.find('a').get('href')
+                                        return url
+
+
+# first argument is the google OTP key, second is the district to download
 if len(sys.argv) > 2:
-    # address = ' '.join(sys.argv[1:])
     otp_key = str(sys.argv[1])
     searchTerms = str(sys.argv[2])
 
@@ -18,37 +34,17 @@ if len(sys.argv) > 2:
 # searchTerms = 'woodbridge'
 
 goSheetUrl = open("C:\\Users\\steveh.AZEROTH\\Desktop\\goz.html")
-
 soup = bs4.BeautifulSoup(goSheetUrl.read())
+notecardsFound = soup.select('div')
 
-notecards = soup.select('div')
-
-url = ''
-
-for card in notecards:
-    if 'notecard' in card.get('class'):
-        if 'SchoolFi' in card.find('b').getText():
-            districts = card.find_all('tr')
-            for district in districts:
-                if district.get('data-type'):
-                    if 'customer' in district.get('data-type'):
-                        if district.find('a'):
-                            districtName = district.find('a').getText()
-                            if searchTerms.lower() in districtName.lower():
-                                if (district.find('a').get('href')):
-                                    url = district.find('a').get('href')
-
+url = get_district_url(notecardsFound)
 if len(url) > 0:
 
     # setting download location'
-    downloadLocation = "C:/k/backups"
     chromeOptions = webdriver.ChromeOptions()
-    # chromeOptions.add_argument('profile.default_content_settings.popups=0')
-    # chromeOptions.add_argument('download.default_directory=C:/k/backups')
+    downloadLocation = "C:/k/backups"
     prefs = {"download.default_directory" : downloadLocation}
     chromeOptions.add_experimental_option("prefs", prefs)
-    # desiredCapabilities = {'prefs': {'download': {'default_directory': 'C:/k/backups/'}}}
-    # path to chromdriver exe
     webdriverPath = "C:/Python\chromedriver.exe"
 
     browser = webdriver.Chrome(executable_path = webdriverPath, chrome_options = chromeOptions)
@@ -62,7 +58,6 @@ if len(url) > 0:
     passwordField.send_keys( "" )
 
     submit = browser.find_element_by_css_selector("input[type='submit']")
-
     submit.submit()
 
     twoFactorPass = browser.find_element_by_id("otp");
@@ -73,11 +68,10 @@ if len(url) > 0:
 
     browser.get(url + "/control?tab1=system&tab2=backups&tab3=files&action=form")
 
-    backupRows = browser.find_elements_by_class_name('listRowHighlight');
-
     schemaName = browser.find_element_by_id("schemaName").get_attribute("value")
-    # schemaName = "sfdunellen"
 
+
+    backupRows = browser.find_elements_by_class_name('listRowHighlight');
     dataRow = backupRows[0]
     docRow = backupRows[1]
 
@@ -90,15 +84,16 @@ if len(url) > 0:
     goodPath = os.path.abspath(downloadLocation)
     os.chdir(goodPath)
 
-    downloadTag = str(datetime.date.today())
+    dateTag = str(datetime.date.today())
 
     pathContents = os.listdir(goodPath)
 
+    # wait until fire is fully downloaded
     while any('crdownload' in dl for dl in pathContents):
         pathContents = os.listdir(goodPath)
 
     for pathItem in pathContents:
-        if downloadTag in pathItem:
+        if dateTag in pathItem:
             if "-docs.dmp.bz2" in pathItem:
                 spliceUntil = pathItem.index("-docs.dmp.bz2")
                 newName = schemaName + pathItem[spliceUntil:]
@@ -113,3 +108,5 @@ if len(url) > 0:
     os.system("start /B start cmd.exe @cmd /k " + command)
 
     webdriver.close()
+
+
