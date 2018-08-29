21dbd98bf9471c2ce7fb86cdeae5646f62169a95
shicks <shicks255@yahoo.com>
2018-08-28 20:58:31 -0400

    getting close to being done

--- CraigslistCrawler/craigslistCrawler.py
+++ CraigslistCrawler/craigslistCrawler.py
@@ -41,16 +41,8 @@ def transformPost(post, category, subcategory):
 
     return cpost.CraigslistPost(url, title, date, location, category, subcategory, price)
 
-listOfPostObjects = []
-categories = cats.categories
-
-for cat in categories:
-    posts = searchACategory(cat[2])
-    listOfPostObjects.extend(list(map(lambda x: transformPost(x, cat[0], cat[1]), posts)))
-
-
-#start email logic
-emailContent = """
+def makeMessage(postChunk):
+    emailContent = """
     <html>
         <head>
             <meta http-equiv="Content=TYPE" content="text/html; charset=utf-8">
@@ -66,50 +58,86 @@ emailContent = """
                 </tr>
             </thead>
             <tbody>
-"""
+    """
 
+    category = ''
+    subcategory = ''
+    for post in postChunk:
+        if category != post.category or subcategory != post.subcategory:
+            category = post.category
+            subcategory = post.subcategory
+            emailContent += '<tr><td colspan="4"><b>' + category + "-" + subcategory + "</b></td></tr>"
+        emailContent += "<tr><td>" + post.postDate + "</td><td>" + post.location + "</td><td>" + post.price + "</td><td>" + post.title + "</td></tr>"
 
-category = ''
-subcategory = ''
-for post in listOfPostObjects:
-    if category != post.category or subcategory != post.subcategory:
-        category = post.category
-        subcategory = post.subcategory
-        emailContent += '<tr><td colspan="4"><b>' + category + "-" + subcategory + "</b></td></tr>"
-
-    emailContent += "<tr><td>" + post.postDate + "</td><td>" + post.location + "</td><td>" + post.price + "</td><td>" + post.title + "</td></tr>"
-
-emailContent += """
+    emailContent += """
             </tbody>
         </table>
         </body>
-    </html>
-"""
+        </html>
+    """
+    return emailContent
 
-subject = "Craigslist Stuff"
-msg = email.message.Message()
-msg['SUBJECT'] = subject
-msg['From'] = 'shicks255@yahoo.com'
-msg['To'] = 'shicks255@yahoo.com'
-msg.add_header('Content-Type', 'text/html')
-msg.set_payload(emailContent)
+def sendEmail(message):
+    subject = "Craigslist Stuff"
+    msg = email.message.Message()
+    msg['SUBJECT'] = subject
+    msg['From'] = 'shicks255@yahoo.com'
+    msg['To'] = 'shicks255@yahoo.com'
+    msg.add_header('Content-Type', 'text/html')
+    msg.set_payload(message)
 
-# try to send the email
-try:
-    smtpObj = smtplib.SMTP('smtp.mail.yahoo.com', 587)
-except smtplib.SMTPAuthenticationError as e:
-    smtplib.quit()
-    sys.exit()
+    # try to send the email
+    try:
+        smtpObj = smtplib.SMTP('smtp.mail.yahoo.com', 587)
+    except smtplib.SMTPAuthenticationError as e:
+        smtplib.quit()
+        sys.exit()
     # log errors here
 
-smtpObj.ehlo()
-smtpObj.starttls()
+    smtpObj.ehlo()
+    smtpObj.starttls()
 
-try:
-    smtpObj.login('shicks255@yahoo.com', '')
-except smtplib.SMTPAuthenticationError as e:
-    # more error logging
+    try:
+        smtpObj.login('shicks255@yahoo.com', '!1')
+    except smtplib.SMTPAuthenticationError as e:
+        # more error logging
+        smtpObj.quit()
+        sys.exit()
+
+    message = msg.as_string().encode("utf-8")
+
+    if len(listOfPostObjects) > 0:
+        try:
+            smtpObj.sendmail('shicks255@yahoo.com', 'shicks255@yahoo.com', message)
+        except smtplib.SMTPSenderRefused as e:
+            sys.exit()
+
     smtpObj.quit()
-    sys.exit()
 
-message = msg.as_string().encode("utf-8")
+
+listOfPostObjects = []
+categories = cats.categories
+
+for cat in categories:
+    posts = searchACategory(cat[2])
+    listOfPostObjects.extend(list(map(lambda x: transformPost(x, cat[0], cat[1]), posts)))
+
+listOfPostChunks = []
+
+keepGoing = True
+counter = 0
+postChunk = []
+for x in range(0, len(listOfPostObjects)):
+    postChunk.append(listOfPostObjects[x])
+    counter += 1
+    if (counter >= 350):
+        listOfPostChunks.append(postChunk)
+        counter = 0
+        postChunk.clear()
+
+for chunk in listOfPostChunks:
+    message = makeMessage(chunk)
+    sendEmail(message)
+
+
+sys.exit()
