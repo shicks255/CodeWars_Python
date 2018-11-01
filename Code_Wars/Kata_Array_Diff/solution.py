f00dc5e75aaad6c13a15eb33190f0d2e6f5c37cf
shicks <shicks255@yahoo.com>
2018-11-01 19:54:29 -0400

    setting up a new problem

---
+++ Code_Wars/Kata_Array_Diff/solution.py
@@ -0,0 +1,27 @@
+# !python 3
+
+# https://www.codewars.com/kata/array-dot-diff/train/python
+
+# Your goal in this kata is to implement a difference function,
+# which subtracts one list from another and returns the result.
+#
+# It should remove all values from list a, which are present in list b.
+#
+# array_diff([1,2],[1]) == [2]
+# If a value is present in b, all of its occurrences must be removed from the other:
+#
+# array_diff([1,2,2,2,3],[2]) == [1,3]
+
+import unittest
+
+def array_diff(a, b):
+    return
+
+
+class TestCase(unittest.TestCase):
+    def testAll(self):
+        self.assert_equals(array_diff([1,2], [1]), [2], "a was [1,2], b was [1], expected [2]")
+        self.assert_equals(array_diff([1,2,2], [1]), [2,2], "a was [1,2,2], b was [1], expected [2,2]")
+        self.assert_equals(array_diff([1,2,2], [2]), [1], "a was [1,2,2], b was [2], expected [1]")
+        self.assert_equals(array_diff([1,2,2], []), [1,2,2], "a was [1,2,2], b was [], expected [1,2,2]")
+        self.assert_equals(array_diff([], [1,2]), [], "a was [], b was [1,2], expected []")
