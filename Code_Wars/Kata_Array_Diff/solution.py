2bea524ffb9612634271c31d3e774a7b87414396
shicks <shicks255@yahoo.com>
2018-11-01 20:03:48 -0400

    finished this one, rather easy with list comprehension

--- Code_Wars/Kata_Array_Diff/solution.py
+++ Code_Wars/Kata_Array_Diff/solution.py
@@ -15,13 +15,13 @@
 import unittest
 
 def array_diff(a, b):
-    return
+    return [x for x in a if x not in b]
 
 
 class TestCase(unittest.TestCase):
     def testAll(self):
-        self.assert_equals(array_diff([1,2], [1]), [2], "a was [1,2], b was [1], expected [2]")
-        self.assert_equals(array_diff([1,2,2], [1]), [2,2], "a was [1,2,2], b was [1], expected [2,2]")
-        self.assert_equals(array_diff([1,2,2], [2]), [1], "a was [1,2,2], b was [2], expected [1]")
-        self.assert_equals(array_diff([1,2,2], []), [1,2,2], "a was [1,2,2], b was [], expected [1,2,2]")
-        self.assert_equals(array_diff([], [1,2]), [], "a was [], b was [1,2], expected []")
+        self.assertEqual(array_diff([1,2], [1]), [2], "a was [1,2], b was [1], expected [2]")
+        self.assertEqual(array_diff([1,2,2], [1]), [2,2], "a was [1,2,2], b was [1], expected [2,2]")
+        self.assertEqual(array_diff([1,2,2], [2]), [1], "a was [1,2,2], b was [2], expected [1]")
+        self.assertEqual(array_diff([1,2,2], []), [1,2,2], "a was [1,2,2], b was [], expected [1,2,2]")
+        self.assertEqual(array_diff([], [1,2]), [], "a was [], b was [1,2], expected []")
