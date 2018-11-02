4905030c7153cb5093a717c43926f4cd8836922e
shicks <shicks255@yahoo.com>
2018-11-02 09:22:59 -0400

    not working yet

--- Code_Wars/Kata_Next_Bigger_Number_With_Same_Digits/solution.py
+++ Code_Wars/Kata_Next_Bigger_Number_With_Same_Digits/solution.py
@@ -3,29 +3,29 @@
 from unittest import TestCase
 
 def next_bigger(n):
-    numbers = str(n)
+    numbers = list(str(n))
 
-    i = len(numbers)-2
-    last = numbers[i+1]
-    while i >= 0:
-        next = numbers[i]
-        if last < next:
-            numbers[i] = next
-            numbers[i-1] = last
-            return int(numbers)
+    i = len(numbers)-1
+    temp = numbers[i]
+    numbers[i] = numbers[i-1]
+    numbers[i-1] = temp
+    if numbers < list(str(n)):
+        i = len(numbers)-2
+        temp = numbers[i]
+        numbers[i] = numbers[i-1]
+        numbers[i-1] = temp
+        return int(''.join(numbers[:i]) + ''.join(sorted(numbers[i:])))
+    return int(''.join(numbers))
 
-        i -= 1
 
+next_bigger(2074)
 
-
-next_bigger(123)
-
-# class Tests(TestCase):
-#     def testAll(self):
-        # self.assert_equals(next_bigger(12),21)
-        # self.assert_equals(next_bigger(513),531)
-        # self.assert_equals(next_bigger(2017),2071)
-        # self.assert_equals(next_bigger(414),441)
-        # self.assert_equals(next_bigger(144),414)
-        # self.assert_equals(next_bigger(2074),2407)
-        # self.assert_equals(next_bigger(1047),1074)
+class Tests(TestCase):
+    def testAll(self):
+        # self.assertEqual(next_bigger(12),21)
+        # self.assertEqual(next_bigger(513),531)
+        # self.assertEqual(next_bigger(2017),2071)
+        # self.assertEqual(next_bigger(414),441)
+        self.assertEqual(next_bigger(144),414)
+        # self.assertEqual(next_bigger(2074),2407)
+        # self.assertEqual(next_bigger(1047),1074)
