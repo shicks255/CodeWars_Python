2f631702177d8565a8fdb0173c93be316b7d4230
shicks <shicks255@yahoo.com>
2018-11-02 09:38:36 -0400

    not entirely sure what's going on with this problem

--- Code_Wars/Kata_Next_Bigger_Number_With_Same_Digits/solution.py
+++ Code_Wars/Kata_Next_Bigger_Number_With_Same_Digits/solution.py
@@ -9,7 +9,7 @@ def next_bigger(n):
     temp = numbers[i]
     numbers[i] = numbers[i-1]
     numbers[i-1] = temp
-    if numbers < list(str(n)):
+    if numbers <= list(str(n)):
         i = len(numbers)-2
         temp = numbers[i]
         numbers[i] = numbers[i-1]
@@ -26,6 +26,7 @@ class Tests(TestCase):
         # self.assertEqual(next_bigger(513),531)
         # self.assertEqual(next_bigger(2017),2071)
         # self.assertEqual(next_bigger(414),441)
-        self.assertEqual(next_bigger(144),414)
+        # self.assertEqual(next_bigger(144),414)
+        self.assertEqual(next_bigger(1234567890),1234567908)
         # self.assertEqual(next_bigger(2074),2407)
         # self.assertEqual(next_bigger(1047),1074)
