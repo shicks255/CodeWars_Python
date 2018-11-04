10d54946eaf6c9b346fe0ebab97a7f02b0e9ca70
shick <shicks255@yahoo.com>
2018-11-04 17:32:09 -0500

    adding doesWordExist in Trie method

--- DataStructures/Trie/Trie.py
+++ DataStructures/Trie/Trie.py
@@ -15,4 +15,20 @@ class Trie():
                 dic[let].leaf = True
             dic = dic[let].letters
 
+    def doesWordExist(self, word):
+        dic = self.root
+        tempNode = None
+        for let in word:
+            if let in dic:
+                tempNode = dic[let]
+                dic = tempNode.letters
+            else:
+                return False
+
+        if tempNode is not None and tempNode.leaf:
+            return True
+
+        return False
+
+
 
--- DataStructures/Trie/driver.py
+++ DataStructures/Trie/driver.py
@@ -7,3 +7,7 @@ trie.addString('pot')
 trie.addString('penis')
 trie.addString('pencil')
 
+print(trie.doesWordExist('pen'))
+print(trie.doesWordExist('peo'))
+print(trie.doesWordExist('pencils'))
+print(trie.doesWordExist('pencil'))
