78b6493d96239f87b7cc8c8301d836a69928ca5b
shicks <shicks255@yahoo.com>
2018-11-05 22:04:52 -0500

    staring binary tree

---
+++ DataStructures/BinaryTree/BinaryTree.py
@@ -0,0 +1,15 @@
+
+from DataStructures.BinaryTree.Node import Node
+
+class BinaryTree():
+
+    def __init__(self, value):
+        self.root = Node(value)
+
+
+    def addNode(self, root, value):
+        if value < root.value:
+
+        if value >= root.value:
+            df
+
---
+++ DataStructures/BinaryTree/Node.py
@@ -0,0 +1,9 @@
+
+class Node():
+
+    def __init__(self,value):
+        self.value = value
+        self.leaf = True
+        self.left = None
+        self.right = None
+
