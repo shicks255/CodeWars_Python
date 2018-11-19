7a6fd2c9dac1b29a1c153fa0d6ee9d048422be26
shicks <shicks255@yahoo.com>
2018-11-18 20:14:04 -0500

    working on heap

--- DataStructures/Heap/Heap.py
+++ DataStructures/Heap/Heap.py
@@ -18,10 +18,35 @@ class Heap():
         else:
             return None
 
+    def getParent(self, index):
+        if index == 0:
+            return None
+        if index % 2 == 0:
+            answerIndex = int((index / 2) - 1)
+        else:
+            answerIndex = int((index-2 / 2))
+        return [answerIndex, self.values[answerIndex]]
+
     def addValue(self, value):
         self.values.append(value)
-        # need heapify up function
+        self.heapifyUp()
 
     def pop(self):
         return self.values.pop()
         # need heapify down
+
+
+    def heapifyUp(self):
+        index = len(self.values)-1
+        while (index > 0):
+            parentTup = self.getParent(index)
+            if parentTup is not None:
+                if parentTup[1] < self.values[index]:
+                    temp = self.values[parentTup[0]]
+                    self.values[parentTup[0]] = self.values[index]
+                    self.values[index] = temp
+            index -= 1
+
+
+    # def heapifyDown(self):
+    #     df
--- DataStructures/Heap/driver.py
+++ DataStructures/Heap/driver.py
@@ -4,7 +4,10 @@ from DataStructures.Heap.Heap import  Heap
 
 heap = Heap(15)
 
-heap.addValue(14)
+heap.addValue(6)
 heap.addValue(5)
+heap.addValue(7)
+
+print(heap.getParent(1))
 
 print(';test')
