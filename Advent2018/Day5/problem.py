85c302196938f3faf239eb6e3a9303c25d1a2ff8
shicks <shicks255@yahoo.com>
2018-12-05 20:48:10 -0500

    part 1 done

--- Advent2018/Day5/problem.py
+++ Advent2018/Day5/problem.py
@@ -4,5 +4,28 @@ import sys
 
 os.chdir(sys.path[0])
 
+def needToRemove(x, y):
+    if x.istitle():
+        return y.istitle() == False
+    if x.istitle() == False:
+        return y.istitle()
+
 with open('input.txt') as input:
-    print(input.read())
+    line = input.read()
+    charList = [x for x in line]
+    start = 0
+    while start < len(charList)-2:
+        x = charList[start]
+        y = charList[start+1]
+        if x.upper() == y.upper():
+            if needToRemove(str(x), str(y)):
+                # print('removing ' + x + ' '  + y)
+                del charList[start:start+2]
+                start -= 2
+                if start < 0:
+                    start = 0
+                    continue
+        start += 1
+
+    print(charList)
+    print(len(charList))
