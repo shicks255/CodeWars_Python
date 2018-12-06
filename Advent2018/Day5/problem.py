a135307125584cc2d8c8b5ce88680407b2f5660e
shicks <shicks255@yahoo.com>
2018-12-05 21:34:14 -0500

    very close with second problem day 2

--- Advent2018/Day5/problem.py
+++ Advent2018/Day5/problem.py
@@ -1,6 +1,7 @@
 
 import os
 import sys
+from collections import Counter
 
 os.chdir(sys.path[0])
 
@@ -27,5 +28,35 @@ with open('input.txt') as input:
                     continue
         start += 1
 
-    print(charList)
-    print(len(charList))
+    # print(charList)
+    # print(len(charList))
+
+    # part 2
+    charListNew = [x.upper() for x in line]
+    counter = Counter(charListNew)
+    print(counter)
+    mostCommon = ['',0]
+    for item, value in counter.items():
+        if value > mostCommon[1]:
+            mostCommon[0] = item
+            mostCommon[1] = value
+
+    someList = [x for x in line if x.upper() != mostCommon[0]]
+
+    start = 0
+    while start < len(someList)-2:
+        x = someList[start]
+        y = someList[start+1]
+        if x.upper() == y.upper():
+            if needToRemove(str(x), str(y)):
+            # print('removing ' + x + ' '  + y)
+                del someList[start:start+2]
+                start -= 2
+            if start < 0:
+                start = 0
+                continue
+        start += 1
+
+    print(someList)
+    print(len(someList))
+    print(counter)
