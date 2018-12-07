fd573633faa7c77e19df0a1c0e195bbb8f6aaa7d
shicks <shicks255@yahoo.com>
2018-12-06 19:07:11 -0500

    making more progress on this problem

--- Advent2018/Day6/problem.py
+++ Advent2018/Day6/problem.py
@@ -1,32 +1,17 @@
 
 import sys
 import os
+from pprint import pprint
 
 os.chdir(sys.path[0])
 
-def findClosest(x, y, grid, steps, checkedList):
-    if (x >= len(grid) or x < 0):
-        return None
-    if (y >= len(grid[x]) or x < 0):
-        return None
+def findPath(x, y, coords):
+    x2 = int(coords[0:coords.index(',')])
+    y2 = int(coords[coords.index(',')+1:])
 
-    point = grid[x][y]
-    if point != '.':
-        return [point[:1], steps]
-
-    checkedList.append([x,y])
-
-    if [x-1,y] not in checkedList:
-        left = findClosest(x-1, y, grid, steps+1, checkedList)
-    if [x+1,y] not in checkedList:
-        right = findClosest(x+1, y, grid, steps+1, checkedList)
-    if [x,y+1] not in checkedList:
-        up = findClosest(x, y+1, grid, steps+1, checkedList)
-    if [x,y-1] not in checkedList:
-        down = findClosest(x, y-1, grid, steps+1, checkedList)
-
-    answer = min(left,right,up,down)
-    print(answer)
+    xDist = abs(x - x2)
+    yDist = abs(y - y2)
+    return xDist + yDist
 
 
 with open('input.txt') as input:
@@ -42,13 +27,31 @@ with open('input.txt') as input:
     for i,coord in enumerate(coords):
         x = int(coord[0:coord.index(',')])
         y = int(coord[coord.index(',')+1:])
-        grid[x][y] = str(i+1) + '' + str(i+1)
-
-    print(grid)
+        grid[x][y] = '*' + str(i+1) + '*'
 
     for x in range(largestX):
         for y in range(largestY):
-            checkedList = []
-            findClosest(x,y, grid, 0, checkedList)
+            bestDistance = None
+            bestCoord = None
+            if '*' in grid[x][y]:
+                continue
+            for i, coord in enumerate(coords):
+                distance = findPath(x, y, coord)
+                if bestDistance is None:
+                    bestDistance = distance
+                    continue
+                if distance == bestDistance:
+                    bestCoord = '.'
+                    break
+                if distance < bestDistance:
+                    bestDistance = distance
+                    bestCoord = i
 
 
+            # grid[x][y] = bestCoord
+            # print(bestCoord)
+
+    # print(grid)
+    # pprint(grid)
+
+    print('\n'.join(['\t'.join([cell for cell in row]) for row in grid]))
