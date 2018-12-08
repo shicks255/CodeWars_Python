d3f8ef2bd4db93b4e00ead321e4cc3df9d3bbad5
shicks <shicks255@yahoo.com>
2018-12-08 01:28:10 -0500

    getting close

--- Advent2018/Day6/problem.py
+++ Advent2018/Day6/problem.py
@@ -1,11 +1,10 @@
 
 import sys
 import os
-from pprint import pprint
 
 os.chdir(sys.path[0])
 
-def findPath(x, y, coords):
+def findPath(y, x, coords):
     x2 = int(coords[0:coords.index(',')])
     y2 = int(coords[coords.index(',')+1:])
 
@@ -13,45 +12,113 @@ def findPath(x, y, coords):
     yDist = abs(y - y2)
     return xDist + yDist
 
+def isInfinite(x, y, grid):
+    thisValue = grid[y][x]
+
+    tempx = x
+    tempy = y-1
+    tempThisValue = thisValue
+
+    while tempy > 0:
+        if grid[tempy][tempx] != grid[y][x].replace('*',''):
+            break
+        tempy -= 1
+    if tempy <= 0:
+        return False
+    else:
+        tempy = y+1
+
+    while tempy != len(grid):
+        if grid[tempy][tempx] != grid[y][x].replace('*',''):
+            break
+        tempy += 1
+    if tempy == len(grid):
+        return False
+    else:
+        tempy = y
+        tempx = x-1
+
+
+    while tempx > 0:
+        if grid[tempy][tempx] != grid[y][x].replace('*', ''):
+            break
+        tempx -= 1
+    if tempx <= 0:
+        return False
+    else:
+        tempx = x + 1
+
+    while tempx < len(grid[y]):
+        if grid[tempy][tempx] != grid[y][x].replace('*',''):
+            break
+        if tempx == len(grid[y]):
+            return False
+        else:
+            return True
+
+def getUsableArea(x, y, grid):
+    value = grid[int(y)][int(x)].replace('*', '')
+    counter = 0
+    for yy, row in enumerate(grid):
+        for xx in grid[yy]:
+            thisValue = grid[int(yy)][int(xx)]
+            if '.' in thisValue:
+                continue
+            if '*' in thisValue:
+                continue
+            if thisValue == value:
+                counter += 1
+    return counter
 
 with open('input.txt') as input:
     coords = [x.rstrip('\n') for x in input.readlines()]
-    xCoords = [int(x[0:x.index(',')]) for x in coords]
-    yCoords = [int(y[y.index(',')+1:]) for y in coords]
+    xCoords = [int(y[0:y.index(',')]) for y in coords]
+    yCoords = [int(x[x.index(',')+1:]) for x in coords]
 
-    largestX = max(xCoords)+1
-    largestY = max(yCoords)+1
+    largestX = max(xCoords) +1
+    largestY = max(yCoords) +1
 
-    grid = [['.' for _ in range(largestY)] for _ in range(largestX)]
+    grid = [['.' for _ in range(largestX)] for _ in range(largestY)]
     # setting up grid coordinates
     for i,coord in enumerate(coords):
         x = int(coord[0:coord.index(',')])
         y = int(coord[coord.index(',')+1:])
-        grid[x][y] = '*' + str(i+1) + '*'
+        grid[y][x] = '*' + str(i+1) + '*'
 
-    for x in range(largestX):
-        for y in range(largestY):
+    for y in range(largestY):
+        for x in range(largestX):
             bestDistance = None
             bestCoord = None
-            if '*' in grid[x][y]:
+            if '*' in grid[y][x]:
                 continue
             for i, coord in enumerate(coords):
-                distance = findPath(x, y, coord)
+                distance = findPath(y, x, coord)
                 if bestDistance is None:
                     bestDistance = distance
+                    bestCoord = i+1
                     continue
                 if distance == bestDistance:
                     bestCoord = '.'
                     break
                 if distance < bestDistance:
                     bestDistance = distance
-                    bestCoord = i
+                    bestCoord = i+1
 
-
-            # grid[x][y] = bestCoord
+            grid[y][x] = str(bestCoord)
             # print(bestCoord)
 
     # print(grid)
     # pprint(grid)
 
-    print('\n'.join(['\t'.join([cell for cell in row]) for row in grid]))
+    print('\n'.join(['\t\t'.join([cell for cell in row]) for row in grid]))
+
+    possibleCoords = [x for x in coords if isInfinite(int(x[0:x.index(',')]), int(x[x.index(',')+1:]), grid)]
+    print(possibleCoords)
+
+    counter = 0
+    for p in possibleCoords:
+        count = getUsableArea(p[0:p.index(',')], p[p.index(',')+1:], grid)
+        if count > counter:
+            counter = count
+
+    print(counter)
