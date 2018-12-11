0f044207865b013272c2d3dd2a4ff59b1addfa82
shicks <shicks255@yahoo.com>
2018-12-10 21:21:28 -0500

    a little progress on day 7 part 2

--- Advent2018/Day7/problem2.py
+++ Advent2018/Day7/problem2.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from string import ascii_uppercase
+from queue import Queue
 
 os.chdir(sys.path[0])
 
@@ -21,12 +22,14 @@ with open('input.txt') as input:
     for line in lines:
         if line[1] not in dependencyMap.keys():
             dependencyMap[line[1]] = []
-
         dependencyMap[line[1]].append(line[0])
 
     letterOrder = []
 
+    alphaNumbers = {'A':61,'B':62,'C':63,'D':64,'E':65,'F':66,'G':67,'H':68,'I':69,'J':70,'K':71,'L':72,'M':73,'N':74,'O':75,'P':76,'Q':77,'R':78,'S':79,'T':80,'U':81,'V':82,'W':83,'X':84,'Y':85,'Z':86}
+
     sorted(dependencyMap)
+    dependencyMapCopy = dict(dependencyMap)
 
     while len(letterOrder) < 26:
         for key,value in dependencyMap.items():
@@ -37,3 +40,38 @@ with open('input.txt') as input:
 
     print(letterOrder)
     print(''.join(x for x in letterOrder))
+
+    # part 2
+    totalTime = 0
+    queue = Queue()
+    lettersUsed = []
+    keepGoing = True
+    workers = [[0,0],[0,0],[0,0],[0,0],[0,0]]
+    lettersToBeWorked = None
+    lettersBeingWorked = []
+    while keepGoing:
+        lettersToBeWorked = [x for x in dependencyMapCopy.keys() if set(dependencyMapCopy[x]).issubset(set(lettersUsed)) and x not in lettersBeingWorked and x not in lettersUsed]
+
+        for x,worker in enumerate(workers):
+            # empty, so put a letter in
+            if worker[0] == 0 and worker[1] == 0:
+                if len(lettersToBeWorked) > 0:
+                    let = lettersToBeWorked.pop(0)
+                    if let not in lettersBeingWorked:
+                        worker[0] = let
+                        worker[1] = alphaNumbers[let]
+                        lettersBeingWorked.append(let)
+            else:
+                worker[1] -= 1
+                if worker[1] == 0:
+                    let = worker[0]
+                    lettersUsed.append(let)
+                    lettersBeingWorked.remove(let)
+                    worker[0] = 0
+                    worker[1] = 0
+                    lettersToBeWorked = [x for x in dependencyMapCopy.keys() if set(dependencyMapCopy[x]).issubset(set(lettersUsed)) and x not in lettersBeingWorked and x not in lettersUsed]
+        if len(lettersUsed) == 26:
+            keepGoing = False
+        totalTime += 1
+
+    print(totalTime)
