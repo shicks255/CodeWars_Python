45a76481c009b2b756904672700990a8a582bea4
shicks <shicks255@yahoo.com>
2018-12-15 21:40:02 -0500

    working on day 9

--- Advent2018/Day9/problem.py
+++ Advent2018/Day9/problem.py
@@ -1,9 +1,21 @@
 
 import os
 import sys
+import re
+from collections import deque
 
 os.chdir(sys.path[0])
 
-
+def parseInput(line):
+    regex = re.compile(r'(\d+)')
+    mo = regex.findall(line)
+    if mo:
+        return [int(mo[0]), int(mo[1])]
 with open("input.txt") as input:
-    print(input.readlines())
+    line = input.readlines()[0]
+    list = deque()
+    playersAndMarbles = parseInput(line)
+
+    print(playersAndMarbles)
+
+
