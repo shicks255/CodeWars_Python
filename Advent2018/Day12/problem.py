dfa889448c001660df50d44af59e75e42b979585
shicks <shicks255@yahoo.com>
2018-12-20 14:59:10 -0500

    so close

--- Advent2018/Day12/problem.py
+++ Advent2018/Day12/problem.py
@@ -4,42 +4,81 @@ import sys
 
 os.chdir(sys.path[0])
 
-def generation():
-    newState = ''
+startingIndex = 0
+
+def generation(initialState):
+    state = list(initialState)
+
+    addToBeginning = False
+    addToEnd = False
+
+    leftTest = '...'
+    for x in state[0:2]:
+        leftTest += str(x)
+    if leftTest in notesMap.keys():
+        addToBeginning = True
+        global startingIndex
+        startingIndex += 1
+
+    rightTest = ''
+    for x in state[len(state)-2:]:
+        rightTest += str(x)
+    rightTest += '...'
+    if rightTest in notesMap.keys():
+        addToEnd = True
+
     for i,x in enumerate(initialState):
-        if i < 2 or i > len(initialState)-2:
-            newState += '.'
-            continue
-        if initialState[i-2:i+3] in notesMap.keys():
-            v = notesMap[initialState[i-2:i+3]]
+        goLeft = i-2
+        goRight = i+3
+        lookingFor = initialState[goLeft:goRight]
+        if i == 0:
+            goLeft = i
+            lookingFor = '..' + initialState[goLeft:goRight]
+        if i == 1:
+            goLeft = i-1
+            lookingFor = '.' + initialState[goLeft:goRight]
+        if i == len(initialState):
+            goRight = i
+            lookingFor = initialState[goLeft:goRight] + '...'
+        if i == len(initialState)-1:
+            goRight = i+1
+            lookingFor = initialState[goLeft:goRight] + '..'
+
+        if lookingFor in notesMap.keys():
+            state[i] = '#'
+            v = notesMap[lookingFor]
             if v == '#':
-                newState += '#'
+                state[i] = '#'
             if v != '#':
-               newState += '.'
+                state[i] = '.'
         else:
-            newState += '.'
-    return newState
+            state[i] = '.'
 
+    if addToBeginning:
+        state.insert(0, '#')
+    if addToEnd:
+        state.append('#')
 
+    return ''.join(state)
 
-with open('testInput.txt') as input:
+with open('input.txt') as input:
     lines = input.readlines()
-    initialState = '..........' + lines[0][lines[0].index('#'):-1] + '...........'
+    initialState = lines[0][lines[0].index('#'):-1]
     initialState.replace('\n', '')
     notes = [x.rstrip('\n') for x in lines[2:]]
     notesMap = {x.split('=>')[0].strip(): x.split('=>')[1].strip() for x in notes}
 
     totalPlants = 0
 
-    for x in range(21):
-        print(x)
-        for xx,yy, in enumerate(initialState):
-            if yy == '#':
-                totalPlants += (xx-10)
-        # totalPlants += len([i for i in initialState if i == '#'])
-        print(totalPlants)
-        print(initialState)
+    for x in range(20):
+        print(str(x) + ' ' +  initialState)
+        # print(initialState)
+        initialState = generation(initialState)
 
-        initialState = generation()
+    print('20 ' + initialState)
+    for xx,yy, in enumerate(initialState):
+        if yy == '#':
+            totalPlants += (xx - startingIndex)
+
 
     print(totalPlants)
